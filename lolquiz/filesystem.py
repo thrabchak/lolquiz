7e5e0810f15c5863e67a46fdd7694ec478992585
Tom Hrabchak <thrabchak@gmail.com>
2017-03-30 15:03:47 -0400

    Ensure data dir is creating before writing to files in it.

--- lolquiz/filesystem.py
+++ lolquiz/filesystem.py
@@ -1,11 +1,12 @@
 import json
-import os.path
+import os
 
 class FileSystemService(object):
   """Class used to encapsulate file system calls."""
   DATA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), '..', "data")
   
   def saveJsonFile(self, jsonData, filename):
+    self.ensureDataDir()
     filePath = os.path.join(self.DATA_FOLDER_PATH, filename)
     with open(filePath, 'w') as jsonFile:
       json.dump(jsonData, jsonFile)
@@ -24,11 +25,17 @@ class FileSystemService(object):
 
   def readFile(self, filename):
     filePath =os.path.join(self.DATA_FOLDER_PATH, filename)
-    with open(filePath) as file:
-      fileString = file.read().strip()
-    return fileString
+    if(os.path.exists(filePath)):
+      with open(filePath) as file:
+        fileString = file.read().strip()
+      return fileString
+    else:
+      print("Error: " + filePath + " does not exist.")
+      raise Exception
+    return ""
 
   def appendFile(self, filename, data):
+    self.ensureDataDir()
     filePath = os.path.join(self.DATA_FOLDER_PATH, filename)
     with open(filePath, 'a') as file:
       file.write(data)
@@ -40,3 +47,9 @@ class FileSystemService(object):
 
   def getEnv(self, envName):
     return os.getenv(envName, "")
+
+  def ensureDataDir(self):
+    directory = os.path.abspath(self.DATA_FOLDER_PATH)
+    if not os.path.exists(directory):
+      print("making data dir")
+      os.makedirs(directory)
